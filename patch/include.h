ef4cf440fe01b613ece9b86a08bfa5c9cf21487a
Nurodayo <cianuroicecream@gmail.com>
2024-01-25 20:31:18 -0300

    mau

---
+++ patch/include.h
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_bstackhoriz.h"
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
